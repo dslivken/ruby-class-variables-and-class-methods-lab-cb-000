b782c28c33d5fb423ea272cd1c62ba232bff60c0
David Slivken <slivken@gmail.com>
2018-06-21 10:26:49 +0000

    automatically backed up by learn

---
+++ testing.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @GENRES = {}
+
+  @@scount = 0
+
+  def initialize (name, artist, genre)
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@scount +=1
+    update_genres(genre)
+  end
+
+  def update_genres (genre)
+    if !@GENRES.keys.include?(genre)
+      @GENRES[genre]=0
+    end
+    @GENRES[genre] +=1
+  end
+
+end
